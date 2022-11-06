539850b527a37e0bba9de4e59933f7e5c9c8b095
massu-159 <magjtwjp@gmail.com>
2022-11-06 14:02:11 +0900

    fix: cors

--- server/index.js
+++ server/index.js
@@ -3,15 +3,28 @@ const app = express();
 const http = require("http");
 const server = http.createServer(app);
 const { Server } = require("socket.io");
-const io = new Server(server); 
+const io = new Server(server, {
+  cors: {
+    origin: ["localhost://3000"],
+  },
+});
+
+
+
 const PORT = 5001;
 
 app.get('/', (req, res) => {
   res.send('<h1>Hello World</h1>');
 });
 
+// クライアントと通信
 io.on("connection", (socket) => {
   console.log("クライアントに接続。");
+
+  socket.on("send_message", (data) => {
+    console.log(data);
+  })
+
   socket.on("disconnect", () => {
     console.log("クライアントとの接続を切断。");
   });
